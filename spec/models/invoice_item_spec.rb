05373b129ea4e76d0f8c282fd6b94d80aca9be95
Jamison Griffith <jamison.griffith@gmail.com>
2021-07-15 13:45:16 -0600

    Add dependent destroy clause to invoice items

---
+++ spec/models/invoice_item_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe InvoiceItem do
+
+  describe 'relationships/validations' do
+    # there are other relationships here, but will filll in later
+    it { should belong_to(:item).dependent(:destroy) }
+  end
+
+end
