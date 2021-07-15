3cf408c22bef3196b0dd3c05359c78f6283544d7
Jamison Griffith <jamison.griffith@gmail.com>
2021-07-15 11:38:57 -0600

    Updates additional model validations for Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  # I assume records can't be updated/created without
+  # a merchant association, but I may be wrong
+  validates_presence_of :name, :description, :unit_price
+  validates_numericality_of :unit_price
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 RSpec.describe Item do
   describe 'relationships/validations' do
     it { should belong_to(:merchant) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_numericality_of(:unit_price) }
   end
 end
