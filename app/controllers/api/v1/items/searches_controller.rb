5ba82e9749fbee067609a2f7b0bc1207b5e6ae53
Jamison Griffith <jamison.griffith@gmail.com>
2021-07-16 14:26:33 -0600

    Implements search endpoints for items and merchant
    Per the design documentation for the project, finding a single resource
    and finding all of a resource are required. Find_by_name returns a
    single matched merchant and find_all returns all matching items. These
    searches are each in their respective search controllers.
    Refactor: create helper method in SearchHelper module for checking empty
    queries.

--- app/controllers/api/v1/items/searches_controller.rb
+++ app/controllers/api/v1/items/searches_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::Items::SearchesController < ApplicationController
 
   def index
-    binding.pry
+    if params[:name].present?
+      @item = Item.find_all(params[:name])
+      json_response(ItemSerializer.new(@item))
+    else
+      json_response({message: "Please provide search parameters"}, :bad_request)
+    end
   end
 
 end
--- app/controllers/api/v1/merchants/searches_controller.rb
+++ app/controllers/api/v1/merchants/searches_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::Merchants::SearchesController < ApplicationController
 
   def show
-    binding.pry
+    if params[:name].present?
+      @merchant = Merchant.find_by_name(params[:name])
+
+      # Helper method would be useful to check whether results are empty or not
+      if @merchant.nil?
+        json_response({data: [], message: "No results"})
+      else
+        json_response(MerchantSerializer.new(@merchant))
+      end
+    else
+      json_response({message: "Please provide search parameters"}, :bad_request)
+    end
   end
 
 end
